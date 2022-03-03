04b8d5452be67f31f9f3d6602ea30ecf965d1e94
markup54 <marzenakupka@tlen.pl>
2022-03-03 09:55:56 +0100

    Dodaj pliki projektów.

---
+++ Kosci3a/Dice.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kosci3a
+{
+    public class Dice : INotifyPropertyChanged
+    {
+        private static readonly string[] images = new string[] {
+            @"Images/kostkapusta.png",
+            @"Images/kostka1.png",
+            @"Images/kostka2.png",
+            @"Images/kostka3.png",
+            @"Images/kostka4.png",
+            @"Images/kostka5.png",
+            @"Images/kostka6.png",
+        };
+
+        private int _value;
+        public int Value 
+        {   get => _value;
+            set
+            {
+                _value = value;
+                if (PropertyChanged != null)
+                {
+                    OnPropertyChanged("Image");
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+                }
+            }
+        }
+
+        public string Image { get => images[Value]; }
+        public float Opacity { get => IsSelected ? 0.5f : 1f;}
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                _isSelected = value;
+                if(PropertyChanged != null)
+                {
+                    OnPropertyChanged("Opacity");
+                    PropertyChanged.Invoke(this,new PropertyChangedEventArgs(nameof(IsSelected)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        void OnPropertyChanged(string prop)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
